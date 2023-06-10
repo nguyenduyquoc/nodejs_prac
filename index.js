02fa238e5c6b2f26f16843508f1582f4f7325ea0
Nguyen Duy Quoc <nguyenduyquoc129829042001@gmail.com>
2023-06-10 08:46:26 +0700

    test

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-ÁC
+const express = require("express");
+const app = express();
+// const database = require("./src/database");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("server is running...");
+})
+
+// app.set("view engine", "ejs");
+// app.use(express.static("public"));
+
+//
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+app.get("/", function (req, res) {
+    res.send("jfsakfsdhf");
+})
+
+
